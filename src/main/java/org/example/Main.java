6aaa2b4804a340b1b5924b3c132203d39237909c
Alexey Khmelev <a.khmelev91@gmail.com>
2021-07-08 21:24:34 +1000

    HTTP Задача №1.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static final String REMOTE_SERVICE_URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = createHttpClient();
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URL);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        List<Cat> cats = mapper.readValue(response.getEntity().getContent(),
+                new TypeReference<List<Cat>>() {
+                })
+                .stream()
+                .filter(value -> value.getUpvotes() != null)
+                .toList();
+        cats.forEach(System.out::println);
+
+    }
+
+    private static CloseableHttpClient createHttpClient() {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .build())
+                .build();
+        return httpClient;
+    }
+}
